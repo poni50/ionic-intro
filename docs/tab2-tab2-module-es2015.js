6de26f5929fb02da2c4de24913b9c79c98f92670
Prodan <proboy599@gmail.com>
2021-01-08 10:37:26 +0100

    pls

---
+++ docs/tab2-tab2-module-es2015.js
@@ -0,0 +1,142 @@
+(window["webpackJsonp"] = window["webpackJsonp"] || []).push([["tab2-tab2-module"],{
+
+/***/ "./node_modules/raw-loader/dist/cjs.js!./src/app/tab2/tab2.page.html":
+/*!***************************************************************************!*\
+  !*** ./node_modules/raw-loader/dist/cjs.js!./src/app/tab2/tab2.page.html ***!
+  \***************************************************************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony default export */ __webpack_exports__["default"] = ("<ion-header [translucent]=\"true\">\r\n  <ion-toolbar>\r\n    <ion-title> Article </ion-title>\r\n  </ion-toolbar>\r\n</ion-header>\r\n\r\n<ion-content>\r\n  <ion-card>\r\n    <img\r\n      src=\"https://www.whitehouse.gov/wp-content/uploads/2017/12/P20170614JB-0303-2-1920x720.jpg\"\r\n    />\r\n    <ion-card-header>\r\n      <ion-card-title> The White House </ion-card-title>\r\n    </ion-card-header>\r\n    <ion-card-content>\r\n      <p>\r\n        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Fuga quaerat\r\n        officiis explicabo temporibus eveniet nisi placeat pariatur, culpa,\r\n        cumque ex cum commodi quod labore voluptatibus magnam eius porro saepe\r\n        est!\r\n      </p>\r\n    </ion-card-content>\r\n  </ion-card>\r\n\r\n  <h4>Comments</h4>\r\n  <ion-list>\r\n    <ion-item>\r\n      <ion-avatar slot=\"start\">\r\n        <img src=\"https://iupac.org/wp-content/uploads/2018/05/default-avatar.png\">\r\n      </ion-avatar>\r\n      <ion-label>\r\n        <h2>Finn</h2>\r\n        <h3>I'm a big deal</h3>\r\n        <p>Listen, I've had a pretty messed up day...</p>\r\n      </ion-label>\r\n    </ion-item>\r\n\r\n    <ion-item>\r\n      <ion-avatar slot=\"start\">\r\n        <img src=\"https://iupac.org/wp-content/uploads/2018/05/default-avatar.png\">\r\n      </ion-avatar>\r\n      <ion-label>\r\n        <h2>Han</h2>\r\n        <h3>Look, kid...</h3>\r\n        <p>I've got enough on my plate as it is, and I...</p>\r\n      </ion-label>\r\n    </ion-item>\r\n\r\n    <ion-item>\r\n      <ion-avatar slot=\"start\">\r\n        <img src=\"https://iupac.org/wp-content/uploads/2018/05/default-avatar.png\">\r\n      </ion-avatar>\r\n      <ion-label>\r\n        <h2>Rey</h2>\r\n        <h3>I can handle myself</h3>\r\n        <p>You will remove these restraints and leave...</p>\r\n      </ion-label>\r\n    </ion-item>\r\n\r\n    <ion-item>\r\n      <ion-avatar slot=\"start\">\r\n        <img src=\"https://iupac.org/wp-content/uploads/2018/05/default-avatar.png\">\r\n      </ion-avatar>\r\n      <ion-label>\r\n        <h2>Luke</h2>\r\n        <h3>Your thoughts betray you</h3>\r\n        <p>I feel the good in you, the conflict...</p>\r\n      </ion-label>\r\n    </ion-item>\r\n  </ion-list>\r\n\r\n  <ion-card>\r\n    <ion-card-header>\r\n      <ion-card-title>\r\n        Add your comment\r\n      </ion-card-title>\r\n    </ion-card-header>\r\n    <ion-card-content>\r\n      <form action=\"\">    \r\n        <ion-label>Name:</ion-label>\r\n        <ion-input type=\"text\"></ion-input>\r\n        <ion-label>Comment:</ion-label>\r\n        <ion-textarea type=\"text\"></ion-textarea>\r\n        <ion-button>Submit</ion-button>\r\n    </form>\r\n    </ion-card-content>\r\n  </ion-card>\r\n</ion-content>\r\n");
+
+/***/ }),
+
+/***/ "./src/app/tab2/tab2-routing.module.ts":
+/*!*********************************************!*\
+  !*** ./src/app/tab2/tab2-routing.module.ts ***!
+  \*********************************************/
+/*! exports provided: Tab2PageRoutingModule */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "Tab2PageRoutingModule", function() { return Tab2PageRoutingModule; });
+/* harmony import */ var tslib__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! tslib */ "./node_modules/tslib/tslib.es6.js");
+/* harmony import */ var _angular_core__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! @angular/core */ "./node_modules/@angular/core/__ivy_ngcc__/fesm2015/core.js");
+/* harmony import */ var _angular_router__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! @angular/router */ "./node_modules/@angular/router/__ivy_ngcc__/fesm2015/router.js");
+/* harmony import */ var _tab2_page__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(/*! ./tab2.page */ "./src/app/tab2/tab2.page.ts");
+
+
+
+
+const routes = [
+    {
+        path: '',
+        component: _tab2_page__WEBPACK_IMPORTED_MODULE_3__["Tab2Page"],
+    }
+];
+let Tab2PageRoutingModule = class Tab2PageRoutingModule {
+};
+Tab2PageRoutingModule = Object(tslib__WEBPACK_IMPORTED_MODULE_0__["__decorate"])([
+    Object(_angular_core__WEBPACK_IMPORTED_MODULE_1__["NgModule"])({
+        imports: [_angular_router__WEBPACK_IMPORTED_MODULE_2__["RouterModule"].forChild(routes)],
+        exports: [_angular_router__WEBPACK_IMPORTED_MODULE_2__["RouterModule"]]
+    })
+], Tab2PageRoutingModule);
+
+
+
+/***/ }),
+
+/***/ "./src/app/tab2/tab2.module.ts":
+/*!*************************************!*\
+  !*** ./src/app/tab2/tab2.module.ts ***!
+  \*************************************/
+/*! exports provided: Tab2PageModule */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "Tab2PageModule", function() { return Tab2PageModule; });
+/* harmony import */ var tslib__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! tslib */ "./node_modules/tslib/tslib.es6.js");
+/* harmony import */ var _ionic_angular__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! @ionic/angular */ "./node_modules/@ionic/angular/__ivy_ngcc__/fesm2015/ionic-angular.js");
+/* harmony import */ var _angular_core__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! @angular/core */ "./node_modules/@angular/core/__ivy_ngcc__/fesm2015/core.js");
+/* harmony import */ var _angular_common__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(/*! @angular/common */ "./node_modules/@angular/common/__ivy_ngcc__/fesm2015/common.js");
+/* harmony import */ var _angular_forms__WEBPACK_IMPORTED_MODULE_4__ = __webpack_require__(/*! @angular/forms */ "./node_modules/@angular/forms/__ivy_ngcc__/fesm2015/forms.js");
+/* harmony import */ var _tab2_page__WEBPACK_IMPORTED_MODULE_5__ = __webpack_require__(/*! ./tab2.page */ "./src/app/tab2/tab2.page.ts");
+/* harmony import */ var _explore_container_explore_container_module__WEBPACK_IMPORTED_MODULE_6__ = __webpack_require__(/*! ../explore-container/explore-container.module */ "./src/app/explore-container/explore-container.module.ts");
+/* harmony import */ var _tab2_routing_module__WEBPACK_IMPORTED_MODULE_7__ = __webpack_require__(/*! ./tab2-routing.module */ "./src/app/tab2/tab2-routing.module.ts");
+
+
+
+
+
+
+
+
+let Tab2PageModule = class Tab2PageModule {
+};
+Tab2PageModule = Object(tslib__WEBPACK_IMPORTED_MODULE_0__["__decorate"])([
+    Object(_angular_core__WEBPACK_IMPORTED_MODULE_2__["NgModule"])({
+        imports: [
+            _ionic_angular__WEBPACK_IMPORTED_MODULE_1__["IonicModule"],
+            _angular_common__WEBPACK_IMPORTED_MODULE_3__["CommonModule"],
+            _angular_forms__WEBPACK_IMPORTED_MODULE_4__["FormsModule"],
+            _explore_container_explore_container_module__WEBPACK_IMPORTED_MODULE_6__["ExploreContainerComponentModule"],
+            _tab2_routing_module__WEBPACK_IMPORTED_MODULE_7__["Tab2PageRoutingModule"]
+        ],
+        declarations: [_tab2_page__WEBPACK_IMPORTED_MODULE_5__["Tab2Page"]]
+    })
+], Tab2PageModule);
+
+
+
+/***/ }),
+
+/***/ "./src/app/tab2/tab2.page.scss":
+/*!*************************************!*\
+  !*** ./src/app/tab2/tab2.page.scss ***!
+  \*************************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony default export */ __webpack_exports__["default"] = ("ion-card img {\n  width: 100%;\n}\n\nh4 {\n  margin-left: 10px;\n  margin-right: 10px;\n  border-bottom: #5e5e5e solid 1px;\n}\n\nion-list {\n  margin-left: 10px;\n  margin-right: 10px;\n}\n/*# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbInNyYy9hcHAvdGFiMi90YWIyLnBhZ2Uuc2NzcyJdLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiQUFDSTtFQUNJLFdBQUE7QUFBUjs7QUFJQTtFQUNJLGlCQUFBO0VBQ0Esa0JBQUE7RUFDQSxnQ0FBQTtBQURKOztBQUtBO0VBQ0ksaUJBQUE7RUFDQSxrQkFBQTtBQUZKIiwiZmlsZSI6InNyYy9hcHAvdGFiMi90YWIyLnBhZ2Uuc2NzcyIsInNvdXJjZXNDb250ZW50IjpbImlvbi1jYXJke1xyXG4gICAgaW1ne1xyXG4gICAgICAgIHdpZHRoOiAxMDAlO1xyXG4gICAgfVxyXG59XHJcblxyXG5oNHtcclxuICAgIG1hcmdpbi1sZWZ0OiAxMHB4O1xyXG4gICAgbWFyZ2luLXJpZ2h0OiAxMHB4O1xyXG4gICAgYm9yZGVyLWJvdHRvbTogcmdiKDk0LCA5NCwgOTQpIHNvbGlkIDFweDtcclxuXHJcbn1cclxuXHJcbmlvbi1saXN0e1xyXG4gICAgbWFyZ2luLWxlZnQ6IDEwcHg7XHJcbiAgICBtYXJnaW4tcmlnaHQ6IDEwcHg7XHJcbn0iXX0= */");
+
+/***/ }),
+
+/***/ "./src/app/tab2/tab2.page.ts":
+/*!***********************************!*\
+  !*** ./src/app/tab2/tab2.page.ts ***!
+  \***********************************/
+/*! exports provided: Tab2Page */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "Tab2Page", function() { return Tab2Page; });
+/* harmony import */ var tslib__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! tslib */ "./node_modules/tslib/tslib.es6.js");
+/* harmony import */ var _angular_core__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! @angular/core */ "./node_modules/@angular/core/__ivy_ngcc__/fesm2015/core.js");
+
+
+let Tab2Page = class Tab2Page {
+    constructor() { }
+};
+Tab2Page.ctorParameters = () => [];
+Tab2Page = Object(tslib__WEBPACK_IMPORTED_MODULE_0__["__decorate"])([
+    Object(_angular_core__WEBPACK_IMPORTED_MODULE_1__["Component"])({
+        selector: 'app-tab2',
+        template: Object(tslib__WEBPACK_IMPORTED_MODULE_0__["__importDefault"])(__webpack_require__(/*! raw-loader!./tab2.page.html */ "./node_modules/raw-loader/dist/cjs.js!./src/app/tab2/tab2.page.html")).default,
+        styles: [Object(tslib__WEBPACK_IMPORTED_MODULE_0__["__importDefault"])(__webpack_require__(/*! ./tab2.page.scss */ "./src/app/tab2/tab2.page.scss")).default]
+    })
+], Tab2Page);
+
+
+
+/***/ })
+
+}]);
+//# sourceMappingURL=tab2-tab2-module-es2015.js.map
